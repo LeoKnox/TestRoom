73a83fa31e27cc15ee0085d74eae45c48c054ef1
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-23 19:50:51 -0700

    Add files via upload

---
+++ ClientApp/src/Room/Rooms.jsx
@@ -0,0 +1,50 @@
+﻿import React, { Component } from 'react';
+
+export class Rooms extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            rooms: [],
+            loading: true
+        }
+    }
+
+    renderAllRoomsTable(rooms) {
+        return (
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Head</th>
+                        <th>Width</th>
+                        <th>Length</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>E</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+
+        let content = this.state.loading ? (
+            <p>
+                <em>Loading ...</em>
+            </p>
+        ) : (
+                this.renderAllRoomsTable(this.state.trips)
+            )
+        return (
+            <div>
+                <h1>All Room</h1>
+                {content}
+            </div>
+            );
+    }
+}
