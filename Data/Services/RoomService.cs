570d3fa948f7e1ce7d76a11c921cfb23da9605e3
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-23 17:28:37 -0700

    Add files via upload

---
+++ Data/Services/RoomService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class RoomService : IRoomService
+    {
+        public void AddRoom(Room rooom)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteRoom(int roomId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Room> GetAllRooms()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Room GetRoomById(int roomId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateRoom(int roomId, Room room)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
