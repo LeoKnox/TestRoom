1c7101a080eb9eef9665ba8dd638c2c213357e03
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-23 17:26:23 -0700

    Add files via upload

---
+++ Data/Services/IRoomServices.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public interface IRoomService
+    {
+        List<Room> GetAllRooms();
+        Room GetRoomById(int roomId);
+        void UpdateRoom(int roomId, Room room);
+        void DeleteRoom(int roomId);
+        void AddRoom(Room rooom);
+    }
+}
