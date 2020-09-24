dd0f819952fa84a5a4d1eb010b8a4c88a8391aff
LeoKnox <LeoKnox@users.noreply.github.com>
2020-09-23 19:29:00 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace WebApplication2
             services.AddDbContext<MvcRoomContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("MvcRoomContext")));
 
-            services.AddTransient<IRoomService, IRoomService>();
+            //services.AddTransient<IRoomService, IRoomService>();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
